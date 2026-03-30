eee7720a70418c2ada9fea97324e9cc8455d1a5e
tetianapitsyk <15092001tp@gmail.com>
2026-03-30 16:47:18 +0300

    test-wrong was created for PEEX

---
+++ tests/test_wrongTest.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from "../testData/fixtures"
+
+
+test('@UI test Appearance - text radiobtns', async ({ page, wikiAppearance }) => {
+    await page.goto('/wiki/Main_Page')
+    await wikiAppearance.largeRB.click()
+    await expect(wikiAppearance.largeRB).toBeChecked()
+    await wikiAppearance.smallRB.click()
+    await expect(wikiAppearance.smallRB).toBeChecked()
+    await wikiAppearance.standartlRB.click()
+    await expect(wikiAppearance.standartlRB).toBeChecked()
+})
+
+
+test('@UI test Appearance - color radiobtns', async ({ page, wikiAppearance }) => {
+    await page.goto('/wiki/Main_Page')
+    await wikiAppearance.automaticColor.click()
+    await expect(wikiAppearance.automaticColor).toBeChecked()
+    await wikiAppearance.lightColor.click()
+    await expect(wikiAppearance.lightColor).toBeChecked()
+    await wikiAppearance.darkColor.click()
+    await expect(wikiAppearance.darkColor).toBeChecked()
+})
